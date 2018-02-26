2460f7e25c335b0e6a8e6987e4fac560fde4dc7f
Aldous Wright <aldous.wright@gmail.com>
2018-02-26 09:55:01 +0800

    Changed box.lwd to bty

--- plot3.R
+++ plot3.R
@@ -1,7 +1,7 @@
-g <-read.table("/home/patrick/Desktop/DOST_DataScience/Module_4/ExData_Plotting1/household_power_consumption.txt", ,
-               sep=";", skip=66637, nrow=2880, col.names = c("Date", "Time", "Global_active_power",
-                                                             "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2",
-                                                             "Sub_metering_3"))
+dataFilePath <- "/home/patrick/Desktop/DOST_DataScience/Module_4/ExData_Plotting1/household_power_consumption.txt"
+g <-read.table(dataFilePath, sep=";", skip=66637, nrow=2880, col.names = c("Date", "Time", "Global_active_power",
+        "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
 png(file="plot3.png", width=480, height=480, units="px")
 g["Datetime"] <- paste(g$Date, g$Time)
 g$Datetime <- strptime(g$Datetime, format="%d/%m/%Y %H:%M:%S")
