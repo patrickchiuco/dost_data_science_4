d6a5ec5c45cf04d35cdd2855ecff127c20f38462
Aldous Wright <aldous.wright@gmail.com>
2018-02-26 10:24:23 +0800

    Removed default values for plot<n>.R. Added instructions to README.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# dost_data_science_4
+#How to run the scripts:
+1. Indicate the datasource's absolute path as dataFilePath's value.
+2. Change your working directory to the path where you want to see the graphs generated.
+3. Run the scripts one by one and check the corresponding graphs for correctness.
--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-dataFilePath <- "/home/patrick/Desktop/DOST_DataScience/Module_4/ExData_Plotting1/household_power_consumption.txt"
+dataFilePath <- ""
 g <-read.table(dataFilePath, sep=";", skip=66637, nrow=2880,
                 col.names = c("Date", "Time", "Global_active_power","Global_reactive_power", "Voltage",
                               "Global_intensity", "Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
