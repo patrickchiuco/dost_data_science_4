93458488ddea8f0a4828851fe9cc42d094ca0be4
Aldous Wright <aldous.wright@gmail.com>
2018-02-26 09:56:07 +0800

    Changed box.lwd to bty

--- plot4.R
+++ plot4.R
@@ -1,7 +1,16 @@
-g <-read.table("/home/patrick/Desktop/DOST_DataScience/Module_4/ExData_Plotting1/household_power_consumption.txt", ,
-               sep=";", skip=66636, nrow=2880, col.names = c("Date", "Time", "Global_active_power",
-                                                             "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2",
-                                                             "Sub_metering_3"))
-png(file="plot1.png", width=480, height=480, units="px")
+dataFilePath <- "/home/patrick/Desktop/DOST_DataScience/Module_4/ExData_Plotting1/household_power_consumption.txt"
+g <-read.table(dataFilePath, sep=";", skip=66636, nrow=2880, col.names = c("Date", "Time", "Global_active_power",
+      "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
+png(file="plot4.png", width=480, height=480, units="px")
+par(mfrow=c(2,2))
+g["Datetime"] <- paste(g$Date,g$Time)
+g$Datetime <- strptime(g$Datetime, format="%d/%m/%Y %H:%M:%S")
+plot(g$Datetime,g$Global_active_power, type="l", ylab="Global Active Power", xlab="")
+plot(g$Datetime,g$Voltage, type="l", ylab="Voltage", xlab="datetime")
+plot(g$Datetime,g$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+points(g$Datetime,g$Sub_metering_2, col="red", type="l")
+points(g$Datetime,g$Sub_metering_3, col="blue", type="l")
+legend(x="topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"), lty=1, bty = 0)
+plot(g$Datetime,g$Global_reactive_power, type="l", ylab="Global_reactive_power", xlab="datetime")
 dev.off()
